e21d8b7e29b884700137ef8660bc66d4b31d9241
Sunwoo Lee <swblaster84@gmail.com>
2022-03-06 10:07:21 -0800

    Fixed the config.

--- classifier/config.py
+++ classifier/config.py
@@ -2,12 +2,12 @@
 All the settings are under an assumption of using 8 processes.
 '''
 oxygen_config = {
-    "batch_size": 100,
+    "batch_size": 50,
     "min_lr": 0.1,
     "max_lr": 0.1,
-    "num_classes": 100,
-    "epochs": 50,
-    "decay_epochs": {25, 40},
+    "num_classes": 10,
+    "epochs": 200,
+    "decay_epochs": [100, 170, 190],
     "input_length": 80,
     "input_path": "./data",
     "weight_decay": 0.0001,
