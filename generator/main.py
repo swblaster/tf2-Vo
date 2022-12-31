fdf3c0313f311b3ae15776435508fa96350aca87
Sunwoo Lee <sunwool@usc.edu>
2022-12-30 18:05:56 -0800

    Gate implemented.

--- generator/main.py
+++ generator/main.py
@@ -35,12 +35,23 @@ class generator:
             exit()
 
         print ("1. Generating random %d trap locations...\n" %(self.num_traps))
-        self.d = abs(np.random.normal(self.mu, self.sigma, self.num_traps))
-        for i in range (len(self.d)):
-            print ("%d\n" %(self.d[i]))
+        self.z = abs(np.random.normal(self.mu, self.sigma, self.num_traps))
+        self.histo, bins = np.histogram(self.z, np.array(range(self.sigma + 100)))
+        histo_sum = sum(self.histo)
+        print ("histo_sum = %d\n" %(histo_sum))
+        #self.electron_distribution = np.zeros((self.sigma))
 
     def bound_range (self):
         print ("2. Bound the aggregation range...\n")
+        self.gate = np.zeros((self.sigma, self.num_traps))
+        for i in range (self.num_samples_per_sigma):
+            for j in range (self.num_traps):
+                for neighbor in range (self.sigma):
+                    distance = abs(neighbor - self.z[j])
+                    if distance <= 50:
+                        self.gate[neighbor][j] = 1
+                    else:
+                        self.gate[neighbor][j] = 0
 
     def calc_noise (self):
         print ("3. Calculate the noise values...\n")
@@ -51,7 +62,7 @@ class generator:
 if __name__ == '__main__':
     gen = generator(cfg.num_traps, cfg.lmd, cfg.tau_0, cfg.num_samples_per_sigma,
                     cfg.unit_sigma, cfg.max_sigma, cfg.unit_freq, cfg.max_freq)
-    gen.sigma = 10
+    gen.sigma = 25
     gen.generate_traps()
     gen.bound_range()
     gen.calc_noise()
