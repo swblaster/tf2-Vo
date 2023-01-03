1d1999f841bba5c78fd4f77030784e5753d8de5a
Sunwoo Lee <sunwool@usc.edu>
2023-01-03 00:59:31 -0800

    Implemented step3.

--- generator/main.py
+++ generator/main.py
@@ -19,6 +19,8 @@ class generator:
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
         self.num_traps = num_traps
+        self.A = 1.0
+        self.f_resolution = 32
         self.mu = 0
         self.sigma = 0
         self.lmd = lmd
@@ -35,11 +37,10 @@ class generator:
             exit()
 
         print ("1. Generating random %d trap locations...\n" %(self.num_traps))
-        self.z = abs(np.random.normal(self.mu, self.sigma, self.num_traps))
-        self.histo, bins = np.histogram(self.z, np.array(range(self.sigma + 100)))
+        self.d = abs(np.random.normal(self.mu, self.sigma, self.num_traps))
+        self.histo, bins = np.histogram(self.d, np.array(range(self.sigma + 100)))
         histo_sum = sum(self.histo)
         print ("histo_sum = %d\n" %(histo_sum))
-        #self.electron_distribution = np.zeros((self.sigma))
 
     def bound_range (self):
         print ("2. Bound the aggregation range...\n")
@@ -55,6 +56,18 @@ class generator:
 
     def calc_noise (self):
         print ("3. Calculate the noise values...\n")
+        self.S = np.zeros((self.max_freq, self.num_traps))
+        t0 = 4e-15
+        lmd = 0.5
+
+        for trap_idx in range (self.num_traps):
+            for freq in range (1, self.max_freq):
+                s = 0
+                for z in range (self.sigma):
+                    exp = np.exp(abs(z - self.d[trap_idx]) / self.lmd)
+                    sub_A = ((t0 * exp ) / (1 + (2 * np.pi * freq * self.f_resolution * (t0 * exp))**2))
+                    s += self.A * self.histo(z) * self.gate(z) * sub_A
+                self.S[freq][trap_idx] = s
 
     def sum_up (self):
         print ("4. Sum up the noise values to create PSD curves...\n")
